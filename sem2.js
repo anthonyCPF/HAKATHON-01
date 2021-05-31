edca97b71957e41ea1751809cd15bb6651c23470
anthony <anthonys.x5x@gmail.com>
2021-05-31 15:02:35 -0500

    subida 2.1

---
+++ sem2.js
@@ -0,0 +1,71 @@
+//ejer 00
+
+
+//ejer 01 listo
+/*
+//se copia el arreglo ? si
+// Que va a mostrar este codigo? 
+let fruits = ["Apple", "Pear", "Orange"]
+let shoppingCart = fruits
+shoppingCart.push("banana")
+console.log(shoppingCart); //
+console.log(fruits.length); // 4
+*/
+
+//ejer 02 listo
+/*
+styles = ["Jazz", "Blues"]
+styles.push("Rock-n-Roll")
+styles.fill("Classic",1,2)
+styles.shift()
+styles.unshift("Rap", "Reggaeton")
+console.log(styles);
+*/
+
+//ejer 03
+
+
+//ejer 04 listo
+/*
+let arr = [5, 3, 8, 1];
+let filtered = arr.filter(ar => {
+    return (ar < 4 )
+})
+console.log(filtered);
+console.log(arr);
+*/
+  
+//ejer 05 listo
+/*
+let arr = ["HTML", "JavaScript", "CSS"];
+let sorted = arr.slice();
+sorted.sort();
+console.log(sorted);
+console.log(arr);
+*/
+
+//ejer 06
+
+
+//ejer 08 listo
+/*
+if (login == "Empleado") {
+    mensaje = "hola";
+} else if (login == "Director") {
+    mensaje = "Saludo";
+} else if (login == "") {
+    mensaje = "Sin inicio de sesion";
+} else {
+    mensaje = "";
+}
+*/
+
+/*
+let login = "Empleado";
+
+login == "Empleado" ? console.log("hola") : console.log(" ");
+login == "Director" ? console.log("saludo") : console.log("");
+login == "" ? console.log("sin inicio de sesion") : console.log("");
+*/
+
+
